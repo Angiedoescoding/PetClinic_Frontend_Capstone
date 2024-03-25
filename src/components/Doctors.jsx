847af6c62f7bae6771aef69b37f9f5a45a567a28
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-24 17:02:04 -0700

    fetching services + styling

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useEffect, useState } from 'react'
 import Vetdog from '../assets/Vetdog.jpg'
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Dogandcatwithavet from '../assets/Dogandcatwithavet.jpg'
 
 function Services() {
+
+  const [services, setServices] = useState();
+
+  useEffect(() => {
+    console.log("Fetching services...");
+    fetch('http://localhost:3000/services')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Something went wrong.');
+      }
+      console.log(response.status);
+
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      setServices(data)
+    })
+  },[])
+
   return (
-    <div>Services</div>
-  )
+    <>
+    <div className='servicesListContainer'>
+    
+    <h1 style={{ color: 'rgb(5, 5, 128)' }}>Services we provide:</h1>
+    {services ? services.map((service) => {
+      return (
+        <div className='serviceList'>
+          <h4 style={{ color: 'green' }}>{service.name}: {service.description}</h4>
+          <p>Cost per procedure: ${service.price}</p>
+          {/* <hr></hr> */}
+        </div>
+      )
+    }) : null}
+    
+    </div>
+    </>  
+    )
 }
 
 export default Services
