19019c192f041a0d3a1eca7e07fee6fd62d08446
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-28 14:07:09 -0700

    delete item added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,6 +4,10 @@ import Modal from './Modal';
 function Services({services, setServices}) {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [editId, setEditId] = useState(null);
+  const [editService, setEditService] = useState(null);
 
   useEffect(() => {
     console.log("Fetching services...");
@@ -31,6 +35,35 @@ function Services({services, setServices}) {
     setIsModalOpen(false);
   }
 
+  const handleEditService = (id) => {
+    fetch('http://localhost:3000/services', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.stringify({name, price}),
+    })
+    .then(response => response.json())
+    .then(updatedService => {
+      setServices(services.map(service => (service._id === editId ? updatedService : service)));
+    })
+    .catch(error => {
+      console.error('Error editing service:', error);
+    });
+  }
+
+  const handleDeleteService = (id) => {
+    fetch(`http://localhost:3000/services/${id}`, {
+      method: 'DELETE',
+    })
+    .then(() => {
+      setServices(services.filter(service => service._id !== id));
+    })
+    .catch(error => {
+      console.error('Error deleting service:', error);
+    });
+  };
+
   return (
     <>
     <div className='servicesListContainer'>
@@ -38,10 +71,11 @@ function Services({services, setServices}) {
       <h1 style={{ color: 'rgb(5, 5, 128)' }}>Services we provide:</h1>
       {services ? services.map((service) => {
         return (
-          <div className='serviceList'>
+          <div className='serviceList' key={service._id}>
             <h4 style={{ color: 'green' }}>{service.name}: {service.description}</h4>
             <p>Cost per procedure: ${service.price}</p>
-            {/* <hr></hr> */}
+            <button onClick={() => handleEditService(service._id.name)}>Edit</button>
+            <button onClick={() => handleDeleteService(service._id)}>Delete</button>
           </div>
         )
       }) : null}
